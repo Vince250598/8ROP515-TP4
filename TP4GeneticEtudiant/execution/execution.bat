c10aafbeeb20c5d014e970f1e70cc8da9fd9bc3c
Vince250598 <vincentgagnon53@gmail.com>
2019-11-22 13:14:41 -0500

    arrange des choses

---
+++ TP4GeneticEtudiant/execution/execution.bat
@@ -0,0 +1,9 @@
+REM L'executable (LaboGA.exe) ainsi que les fichiers de la DLL (GeneticDLL.dll et GeneticDLL.lib)
+REM doivent etre dans le meme repertoire que le fichier .txt
+
+REM *** Liste des paramètres À AJUSTER ET À MODIFIER*** 
+
+REM 1)EXECUTABLE 2)NOM_FICHIER 3)TAILLEPOP 4)PROB_Cr 5)PROB_Mut 6)NB_EVAL_MAX 7)H
+
+LaboGA.exe	FichierA.txt	10	0.1	0.1	100	0.2
+LaboGA.exe	FichierA.txt	10	0.1	0.1	100	0.2
